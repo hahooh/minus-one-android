c8536fb06b7fa54522f37bbfd91c551c0898528c
Jiwan Kim <hahooh@gmail.com>
2024-07-30 01:09:48 +0900

    testing

--- app/src/main/java/com/example/minusone/MainActivity.kt
+++ app/src/main/java/com/example/minusone/MainActivity.kt
@@ -6,6 +6,7 @@ import android.widget.Button
 import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.example.minusone.caller.Apicaller
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,10 +18,17 @@ class MainActivity : AppCompatActivity() {
         val emailInput = findViewById<EditText>(R.id.emailInput)
         val passwordInput = findViewById<EditText>(R.id.passwordInput)
         loginButton.setOnClickListener {
-            var email = emailInput.text
-            var password = passwordInput.text
-            Log.i("MainActivity", email.toString())
-            Log.i("MainActivity", password.toString())
+            this.login(emailInput.toString(), passwordInput.toString())
         }
+    }
+
+    private fun login(email: String, password: String) {
+        Log.i("email!", email)
+        Log.i("password!", password)
+        val caller = Apicaller("http://10.0.2.2:8080/v1")
+        val body = mutableMapOf<String,String>()
+        body["email"] = email
+        body["password"] = password
+        caller.post("/login", body)
     }
 }
